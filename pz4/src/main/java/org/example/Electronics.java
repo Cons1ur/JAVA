2e1bacd02b34a98b72a40a4aa64c3a191148dd8e
Cons1ur <figerft777@gmail.com>
2024-11-22 19:18:02 +0200

    Delete pz4 directory

--- pz4/src/main/java/org/example/Electronics.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-import lombok.Builder;
-
-public class Electronics extends Product {
-    @Builder
-    public Electronics(String name, Double price, String description) {
-        super(name, price, description);
-    }
-}
